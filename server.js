b14c0344105eb2a9add2c73bdd63e2ca1946133d
René Rendón <rene.d.rendon@unitec.edu>
2018-10-26 01:11:16 -0600

    Added CRUD for companies

---
+++ server.js
@@ -0,0 +1,30 @@
+//server.js
+const hapi = require('hapi');
+const server = new hapi.Server();
+const mongoose = require('mongoose');
+const mongoDbUri = 'mongodb://localhost:27017/hapi_db';
+const CompanyRoutes = require('./routes/company.routes');
+
+//connect with mongoDB
+mongoose.connect(mongoDbUri, {
+	useMongoClient: true
+});
+mongoose.connection.on('connected', () => {
+	console.log(`app is connected to ${mongoDbUri}`);
+});
+mongoose.connection.on('error', err => {
+	console.log('error while connecting to mongodb', err);
+});
+server.connection({
+	host: 'localhost',
+	port: '3000'
+});
+
+server.route(CompanyRoutes);
+
+server.start(err => {
+	if (err) {
+		throw err;
+	}
+	console.log(`Server Running at PORT ${server.info.port}`);
+});
